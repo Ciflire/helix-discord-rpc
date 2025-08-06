9190da9cfabbe585365768e5bee9632186e25f15
VESSE Léo <leovesse@gmail.com>
2025-08-06 19:37:26 +0200

    chore : updated the cog

--- cog.scm
+++ cog.scm
@@ -1,24 +1,7 @@
 (define package-name 'helix-discord-rpc)
-(define version "0.1.6")
+(define version "0.1")
 
 (define dependencies '())
 
 (define dylibs
-  '((#:name "helix-discord-rpc"
-     #:urls
-     (
-      (
-       #:platform
-       "x86_64-windows"
-       #:url
-       "https://github.com/nik-rev/ghost-text.hx/releases/download/v0.1.6/ghost_text.dll")
-      (
-       #:platform
-       "x86_64-macos"
-       #:url
-       "https://github.com/nik-rev/ghost-text.hx/releases/download/v0.1.6/libghost_text.dylib")
-      (
-       #:platform
-       "x86_64-linux"
-       #:url
-       "https://github.com/nik-rev/ghost-text.hx/releases/download/v0.1.6/libghost_text.so")))))
+  '((#:name "helix-discord-rpc")))
