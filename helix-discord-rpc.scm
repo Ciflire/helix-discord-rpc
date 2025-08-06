77fe98b0f05798d2179f1455c60fe367f58e0638
VESSE Léo <leovesse@gmail.com>
2025-08-06 19:08:09 +0200

    feat : auto connection, basic activity, some icons...
    fix : removing undesired hook

--- helix-discord-rpc.scm
+++ helix-discord-rpc.scm
@@ -1,15 +1,31 @@
 ;; Builtin imports
 (require-builtin steel/strings)
+(require-builtin steel/time)
 
 ;; Cogs imports
 (require "helix/editor.scm")
 
 ;; dylibs imports
-(#%require-dylib "libhelix-discord-rpc"
+(#%require-dylib "libhelix_discord_rpc"
   (only-in
     DiscordRPC::new
-    DiscordRPC::connect))
+    DiscordRPC::connect
+    DiscordRPC::set_activity
+    DiscordRPC::set_idle))
 
 (define server (DiscordRPC::new))
+(define is-connected #false)
 
-(register-hook! 'document-opened (lambda (doc-id) (DiscordRPC::connect server)))
+(register-hook! 'document-focus-lost (lambda (doc-id) (if is-connected (begin (DiscordRPC::set_activity server (to-string (editor-document->path doc-id)) (helix-find-workspace))))))
+
+;;@docs
+; Connects the server to discord's websocket
+(define (discord-rpc-connect) (
+                               if
+                               is-connected
+                               "Websocket already connected"
+                               (begin (DiscordRPC::connect server) (set! is-connected #true) "Websocket connected")))
+
+(discord-rpc-connect)
+
+(provide discord-rpc-connect)
