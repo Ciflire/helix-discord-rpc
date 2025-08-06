f498c4f2f3c9eca1db7311cae744a9d96a9597b0
VESSE Léo <leovesse@gmail.com>
2025-08-06 20:22:37 +0200

    chore : commenting code

--- src/assets.rs
+++ src/assets.rs
@@ -1,10 +1,15 @@
 use std::{error::Error, path::Path};
 
+/// This function return the corresponding asset id which is the name of the file in the discord developper portal
+/// We match first on the filename if it is known it is returned, else we match on the extension.
+/// If the extension is unknown then it falls back to helix icon
+/// Note that you are not able to know in advance the asset id, only @ciflire can, so if you have any remark please open an issue
 pub fn get_asset(filename: String) -> String {
     let extension = Path::new(&filename).extension().unwrap().to_str().unwrap();
     let res = match filename.as_str() {
         "Cargo.toml" => "cargo".to_string(),
         "Cargo.lock" => "cargo".to_string(),
+        "hyprland.conf" => "hyprland".to_string(),
         _ => String::new(),
     };
     if !res.is_empty() {
@@ -20,6 +25,6 @@ pub fn get_asset(filename: String) -> String {
 
         "scm" => "lisp".to_string(),
 
-        _ => "python".to_string(),
+        _ => "helix".to_string(),
     }
 }
